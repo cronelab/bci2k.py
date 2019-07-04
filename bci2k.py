021691b8bcd30aa5a1ac44eef689a1c417fc9e4d
Chris <17508521+TheBrainChain@users.noreply.github.com>
2019-07-04 00:35:08 -0400

    data parsing

--- bci2k.py
+++ bci2k.py
@@ -4,13 +4,25 @@ import websockets
 
 async def dataConnector():
     async with websockets.connect('ws://localhost:20100') as websocket:
-        # await websocket.send(f"")
+        stateFormat = await websocket.recv()
+        # print(stateFormat.decode("utf-8"))
+        print("")
+        signalProperties = await websocket.recv()
+        # print(signalProperties.decode("utf-8"))
+        print("")
         data = await websocket.recv()
-        print(data)
-        data1 = await websocket.recv()
-        print(data1)
-        data2 = await websocket.recv()
-        print(data2)
+        if data[0] == 4:  # Visualization and Brain Signal Data Format
+            if data[1] == 1:  # Signal
+                if(data[2] == 2):  # float32
+                    # number of channels
+                    print("number of channels: ")
+                    print(data[3])
+                    # print(data[4])  # + 256 channels
+                    # number of elements
+                    print("number of elements: ")
+                    print(data[5])
+                    # print(data[6])  # + 256 elements
+
 
 while True:
     asyncio.get_event_loop().run_until_complete(dataConnector())
